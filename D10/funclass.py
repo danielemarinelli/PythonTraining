9d704ffaa4e458521b2178675a527c59de54a933
Daniele <danielemarinelli1387@gmail.com>
2026-04-13 17:53:59 +0200

    refactor funclass.py file

--- D10/funclass.py
+++ D10/funclass.py
@@ -1,10 +1,50 @@
 
+# function with no parameters and no return value
+def greetings():
+    print("Hello, it's a sunny day!")
 
+# calling the function outside the block
+greetings()
+greetings()
+greetings()
+print('<<<<<>>>>>>')
 
-def calc():
-    a = 4
-    b = 13
-    return a*b
+# function with one parameter and no return value
+def day(name):
+    print('have a great ',name)
+
+day("monday")
+day("Tuesday")
+
+# function with 2 parameters and a return value
+def calc(a,b):
+   return a*b
+value = calc(10,2)
+print(value)
+
+# function returns 'None' , 2 ways below:
+def myfun():
+    return
+print('<<<<<>>>>>>')
+print(myfun())
+def myfun2():
+    i =100
+print(myfun2())
+print('<<<<<>>>>>>')
+
+#Arbitrary arguments
+def sum_of_numbers(*numbers):
+    tot = 0;
+    for i in numbers:
+        tot += i
+    return tot
+
+print(sum_of_numbers(1,2,3))
+print(sum_of_numbers(2,30))
+print(sum_of_numbers(2,30,9,13))
+print(sum_of_numbers(21,330,90,173))
+print('<<<<<>>>>>>')
+
 
 def openfile():
     file = open('example.txt')
@@ -12,7 +52,4 @@ def openfile():
     print(file.read())
     file.close()
 
-
-
-print(calc())
 openfile()
