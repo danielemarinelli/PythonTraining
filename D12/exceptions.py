e19d57c6c260609399af742071c31cff87ac2564
Daniele <danielemarinelli1387@gmail.com>
2026-04-16 15:15:40 +0200

    refactor trycatch.py and exceptions.py

--- D12/exceptions.py
+++ D12/exceptions.py
@@ -1,8 +1,69 @@
+#Exceptions
+
+#x=6
+#res=6/0  Exception ---> ZeroDivisionError
+#print(res)
+
+print("start!!")
+x="10"
+#print(x+5)  another exception: add string and num not possible
+
+try:
+    print(x+5)
+except:
+    print("An exception occured...")
+print("finish!!")
 
 
+# Multiple exceptions
+
+try:
+    print(abc)
+except NameError:
+    print("Variable abc is not defined...")
+except:
+    print("other exceptions...")
+
+# Try block with else
+
+try:
+    print("Checking exceptions")
+    100/0
+except:
+    print("something went wrong...")
+else:
+    print("Nothing went wrong today")
+
+
+# Finally keyword
+try:
+    print(b)
+except:
+    print("something went wrong again...!!!")
+finally:   # finally block always is executed
+    print("here is the finally block")
+
+# combination try, except, else, finally
+try:
+    n=int(input("Enter a value: "))
+    res=100/n
+except ZeroDivisionError:
+    print("Don't divide by Zero....")
+except ValueError:
+    print("Enter a valid number...")
+else:
+    print(res)
+finally:
+    print("Exceptions completed!!")
+
+
+
+
+
+#####################################
 ItemsInCart = 0
 # 2 items will be added in cart from automation script
-
+# with raise keyword you can send your own exception (mostly developers use it)
 if ItemsInCart !=2:
     raise Exception("ItemsInCart must be 2.....")
 
--- D12/trycatch.py
+++ D12/trycatch.py
@@ -10,7 +10,25 @@ try:
     with open('filelog.txt', 'r') as reader:
         reader.read()
 except Exception as e:
-    print(e)  # python error displyed
+    print(e)  # python error display
 
 finally:
     print("Finished!!!!")  # line always executed
+
+
+# nested try-catch blocks
+# one example is line 23 without specify the mode (external exception will run)
+# another example is -> change mode from w to r  (nested exception will run)
+try:
+    file=open('filelog.txt','w')
+    try:
+        file.write("hello world")
+    except:
+        print("Something went wrong when writing to file")
+    finally:
+        file.close()
+except:
+    print("Something went wrong when opening the file")
+
+
+
