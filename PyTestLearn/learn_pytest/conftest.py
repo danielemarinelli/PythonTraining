bd2b53f5935fc7a7aba86673c278ed62088346e3
Daniele <danielemarinelli1387@gmail.com>
2026-04-17 17:05:57 +0200

    refactor coftest.py

--- PyTestLearn/learn_pytest/conftest.py
+++ PyTestLearn/learn_pytest/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
 # re-use fixture in all modules. It's a common fixture and available from all
-# the test modules. The fix name of file must be conftest.py , now this fixture
-# contains all fixtures that are available for all modules across all framework
+# the test modules. The fix name of file must be conftest.py , now this file
+# contains all fixtures that are available for all modules across all package
 # ( the two modules test_logins.py and test_signups.py can access this fixture)
 
 @pytest.fixture()
