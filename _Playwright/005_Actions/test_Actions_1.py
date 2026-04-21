d6c2303b25421b4402a3c4cb1819cb674bc311bb
Daniele <danielemarinelli1387@gmail.com>
2026-04-21 09:42:38 +0200

    added test_Actions1.py

---
+++ _Playwright/005_Actions/test_Actions_1.py
@@ -0,0 +1,31 @@
+
+from playwright.sync_api import Page, expect
+
+def test_insert_inputBox(page: Page):
+    page.goto("https://practice-automation.com/form-fields/")
+    expect(page.locator("#name-input")).to_be_visible()
+    expect(page.locator("//input[@type='password']")).to_be_visible()
+    page.locator("#name-input").fill("daniele_QAteam")
+    page.locator("//input[@type='password']").fill("01234")
+
+    page.wait_for_timeout(2000)
+
+
+def test_check_radioBtn(page: Page):
+    page.goto("https://practice-automation.com/form-fields/")
+    radio_btns = page.locator("//input[@type='radio']")
+    print(radio_btns.count())
+    colors = ['Red', 'Green', 'Yellow', 'Blue', '#FFC0CB' ]
+    colors_radio = []
+    for color in colors:
+        radio = page.get_by_label(color)
+        colors_radio.append(radio)
+    print("total number of radio buttons:", len(colors_radio))
+    print(colors_radio)
+    page.get_by_label("Yellow").check()
+    page.wait_for_timeout(2000)
+
+
+
+
+
